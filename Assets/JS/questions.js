5b3b2f56c2998985257264bc32ecf2c09f15077a
GhostoftheMill <kevinmichaelwhite@gmail.com>
2022-10-11 12:23:51 -0400

    Populated questions in js

---
+++ Assets/JS/questions.js
@@ -0,0 +1,89 @@
+// Questions for quiz
+var questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+  {
+    title:
+    'HTML is short for:',
+    choices: ['HyperText Markdown Language ', 'HyperText Markup Language', 'Cascading Style Sheet', 'None of the above'],
+    answer: 'HyperText Markup Language',
+  },
+  {
+    title:
+      'Which symbol is used to enclose HTML tags?',
+    choices: ['{}', '[]', '+-', '<>'],
+    answer: '<>',
+  },
+  {
+    title:
+      'CSS stands for:',
+    choices: ['justice & equality!', 'Computer Super Styling', 'Cascading Style Sheet', 'Console Serial Shower'],
+    answer: 'Cascading Style Sheet',
+  },
+  {
+    title:
+      'In CSS what pseudo-class selector is used to select the highest-level parent of a given specification?',
+    choices: [':ultimate', ':mine', ':first', ':root'],
+    answer: ':root',
+  },
+  {
+    title:
+      'Which keyword is used to define a variable in JavaScript?',
+    choices: ['var', 'let', 'Both var and let', 'None of the Above'],
+    answer: 'Both var and let',
+  },
+  {
+    title:
+      'The innermost layer of the CSS box model is:',
+    choices: ['Content', 'Margin', 'Padding', 'Border'],
+    answer: 'Content',
+  },
+  {
+    title:
+      'Which header tag in HTML results in the largest font? ',
+    choices: ['headmaster', 'h1', 'h10', 'h6'],
+    answer: 'h1',
+  },
+  {
+    title:
+      'The outermost layer of the CSS box model is:',
+    choices: ['Border', 'Margin', 'Padding', 'Content'],
+    answer: 'Margin',
+  },
+  {
+    title:
+      'The HTML element used to embed executable code or date is:',
+    choices: ['<script>'>, '<code>', '<execute>', '<choose>'],
+    answer: '<script>',
+  },
+];
